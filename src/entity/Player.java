6e2326969700cd16a77c09512769bf2bb99c39dd
guilhermeRizzatto <guimarquesrz@gmail.com>
2023-12-13 22:21:25 -0300

    Add sprites player

---
+++ src/entity/Player.java
@@ -0,0 +1,129 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+public class Player extends Entity {
+
+    boolean hasSprite3;
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void update() {
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+
+            if (keyH.upPressed == true) {
+                hasSprite3 = true;
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed == true) {
+                hasSprite3 = true;
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed == true) {
+                hasSprite3 = false;
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed == true) {
+                hasSprite3 = false;
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter > 10) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2 && hasSprite3 == true) {
+                    spriteNum = 3;
+                } else if (spriteNum == 2 && hasSprite3 == false){
+                    spriteNum = 1;
+                } else if (spriteNum == 3) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        } else spriteNum = 1;
+    }
+
+    public void draw(Graphics2D g2) {
+
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1)
+                    image = up1;
+                if (spriteNum == 2)
+                    image = up2;
+                if (spriteNum == 3)
+                    image = up3;
+                break;
+            case "down":
+                if (spriteNum == 1)
+                    image = down1;
+                if (spriteNum == 2)
+                    image = down2;
+                if (spriteNum == 3)
+                    image = down3;
+                break;
+            case "left":
+                if (spriteNum == 1)
+                    image = left1;
+                if (spriteNum == 2)
+                    image = left2;
+                if (spriteNum == 3)
+                    image = left1;
+                break;
+            case "right":
+                if (spriteNum == 1)
+                    image = right1;
+                if (spriteNum == 2)
+                    image = right2;
+                if (spriteNum == 3)
+                    image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
+    }
+
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerUp1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerUp2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerUp3.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerDown1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerDown2.png"));
+            down3 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerDown3.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerLeft1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerLeft2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerRight1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/PlayerSprites/PlayerRight2.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,39 +7,41 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
-	
-	//screen Settings
-	final int originalTileSize = 16; //16x16 tile
+import entity.Player;
+
+public class GamePanel extends JPanel implements Runnable {
+
+	// screen Settings
+	final int originalTileSize = 16; // 16x16 tile
 	final int scale = 3;
-	
-	final int tileSize = originalTileSize * scale; //48x48 tile
+
+	public final int tileSize = originalTileSize * scale; // 48x48 tile
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol; //768 pixels
-	final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
-	//fps
+	// fps
 	int FPS = 60;
-	
+
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	Player player = new Player(this, keyH);
 
-	//set player's default position
+	// set player's default position
 	int playerX = 100;
 	int playerY = 100;
 	int playerSpeed = 4;
-	
+
 	public GamePanel() {
-		
+
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
 	}
-  
-	
+
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start();
@@ -47,62 +49,51 @@ public class GamePanel extends JPanel implements Runnable{
 
 	@Override
 	public void run() {
-		
-		
-		double drawInterval = 1000000000/FPS;
+
+		double drawInterval = 1000000000 / FPS;
 		double delta = 0;
 		long lastTime = System.nanoTime();
 		long currentTime;
 		long timer = 0;
 		int drawCount = 0;
-		
-		
-		while(gameThread != null) {
-			
+
+		while (gameThread != null) {
+
 			currentTime = System.nanoTime();
 
 			delta += (currentTime - lastTime) / drawInterval;
 			timer += (currentTime - lastTime);
 			lastTime = currentTime;
 
-			if(delta >= 1){
+			if (delta >= 1) {
 				update();
 				repaint();
 				delta--;
 				drawCount++;
 			}
-				
-			if(timer >= 1000000000){
+
+			if (timer >= 1000000000) {
 				System.out.println("fps: " + drawCount);
 				drawCount = 0;
 				timer = 0;
 			}
 		}
-		
+
 	}
-	
+
 	public void update() {
-		if(keyH.upPressed == true){
-			playerY -= playerSpeed;
-		} else if (keyH.downPressed == true){
-			playerY += playerSpeed;
-		} else if (keyH.leftPressed == true){
-			playerX -= playerSpeed;
-		} else if (keyH.rightPressed == true){
-			playerX += playerSpeed;
-		}
+		player.update();
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		
+
 		super.paintComponent(g);
-		
-    Graphics2D g2 = (Graphics2D)g;
-    g2.setColor(Color.white);
 
-    g2.fillRect(playerX, playerY, tileSize, tileSize);
+		Graphics2D g2 = (Graphics2D) g;
 
-    g2.dispose();
+		player.draw(g2);
+
+		g2.dispose();
 	}
 
 }
